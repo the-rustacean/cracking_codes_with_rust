cd9312cb127d25222adbb87124089424049672ec
yphilkim <yphilkim@inter-elec.com>
2025-08-26 17:32:07 +0900

    fixed

--- the_reverse_cipher/src/main.rs
+++ the_reverse_cipher/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let message = "Three can keep a secret, if two of them are dead";
+    
+    let mut translated = String::new();
+    
+    let mut i = message.len();
+    
+    while i > 0 {
+        translated.push_str(&message[i-1..i]);
+        
+        i = i - 1;
+    }
+    
+    println!("{}", translated);
 }
