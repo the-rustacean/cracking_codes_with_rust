57c1a51438b8166198296fe3db6e4b5a797bf0bb
yphilkim <yphilkim@inter-elec.com>
2025-08-27 09:05:21 +0900

    fixed

--- the_reverse_cipher/src/main.rs
+++ the_reverse_cipher/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     let message = "Three can keep a secret, if two of them are dead";
-    
+
     let mut translated = String::new();
-    
+
     let mut i = message.len();
-    
+
     while i > 0 {
         translated.push_str(&message[i-1..i]);
-        
+
         i = i - 1;
     }
-    
+
     println!("{}", translated);
 }
