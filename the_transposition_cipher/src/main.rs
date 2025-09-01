5bd1d79ecd24ff955487fe4fd1a71ff6ec8d4074
yphilkim <yphilkim@inter-elec.com>
2025-09-01 17:37:29 +0900

    fixed

--- the_transposition_cipher/src/main.rs
+++ the_transposition_cipher/src/main.rs
@@ -1,3 +1,25 @@
+// use rand::Rng;
+use rand::seq::SliceRandom;
+
 fn main() {
-    println!("Hello, world!");
+
+    for i in 0..20 {
+        let alphbet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let mut message = String::new();
+
+        for _ in 0..(i + 1) {
+            message.push_str(alphbet);
+        }
+
+        // println!("{}", message);
+
+
+        // let mut rng = rand::rng();
+        // let mut bytes = "".to_string().into_bytes();
+        // bytes.shuffle(&mut rng);
+        // let str = String::from_utf8(bytes).unwrap();
+
+
+
+    }
 }
