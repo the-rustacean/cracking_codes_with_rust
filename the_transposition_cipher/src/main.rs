f298517286d1510422502cb6270bfd280cb53e42
yphilkim <yphilkim@inter-elec.com>
2025-09-03 16:56:59 +0900

    fixed

--- the_transposition_cipher/src/main.rs
+++ the_transposition_cipher/src/main.rs
@@ -1,25 +1,39 @@
-// use rand::Rng;
+use std::process;
+use rand::Rng;
 use rand::seq::SliceRandom;
+use the_transposition_cipher::{decrypt_message, encrypt_message};
 
 fn main() {
 
-    for i in 0..20 {
-        let alphbet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let mut rng = rand::rng();
+
+    for test_number in 1..=20 {
+        let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         let mut message = String::new();
 
-        for _ in 0..(i + 1) {
-            message.push_str(alphbet);
+        let text_times = rng.random_range(4..40);
+
+        for _ in 0..text_times {
+            message.push_str(text);
         }
 
-        // println!("{}", message);
+        let mut bytes = message.into_bytes();
+        bytes.shuffle(&mut rng);
+        let str = String::from_utf8(bytes).unwrap();
 
+        println!("Test #{}: {}", test_number, &str[..50]);
 
-        // let mut rng = rand::rng();
-        // let mut bytes = "".to_string().into_bytes();
-        // bytes.shuffle(&mut rng);
-        // let str = String::from_utf8(bytes).unwrap();
+        let max_key = str.len() / 2;
 
+        for key in 1..max_key {
+            let encrypted = encrypt_message(key, &str);
+            let decrypted = decrypt_message(key, &encrypted);
 
-
+            if str != decrypted {
+                println!("Mismatch with key {} and message {}", key, str);
+                process::exit(1);
+            }
+        }
     }
+    println!("Transposition cipher test passed.");
 }
